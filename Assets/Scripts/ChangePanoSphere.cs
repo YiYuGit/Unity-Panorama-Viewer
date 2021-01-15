2d0d5ac6851c9dfd0190c85d48aaaad68345bd21
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-01-15 14:50:40 -0500

    Upload

---
+++ Assets/Scripts/ChangePanoSphere.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePanoSphere : MonoBehaviour
+{
+
+    // Attach to an empty object, put all panoramic sphere here in the showing sequence
+
+    [Header("Change number, drop panospheres here")]
+
+    public GameObject[] panoSphere;
+
+    [Header("Press key 'space' to switch spheres")]
+
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // On start, Only keep the first panosphere active
+        for (int j =1; j < panoSphere.Length; j++ )
+        {
+            panoSphere[j].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for Quiting the application
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                Application.Quit();
+        }
+
+
+        // for switching the pano spheres
+        if (Input.GetKeyDown("space"))
+        {
+            // Close all panosphere
+            for (int j = 0; j < panoSphere.Length; j++)
+            {
+                panoSphere[j].SetActive(false);
+            }
+            // turn on the i panosphere
+            panoSphere[i].SetActive(true);
+            Camera.main.transform.position = panoSphere[i].transform.position;
+            if (i < panoSphere.Length)
+            {
+                i++;
+            }
+
+            if (i >= panoSphere.Length)
+            {
+                i = 0;
+            }
+                
+        }
+    }
+}
+    
---
+++ Assets/Scripts/SimpleCameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System;
+
+public class SimpleCameraController : MonoBehaviour
+{
+    // Control the direction of the camera
+    Vector2 rotation = new Vector2(0, 0);
+
+    // Adjust the speed the direction rotation
+    public float speed = 3;
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+    }
+}
